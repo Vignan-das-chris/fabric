df9da8c3c9e26e2500bcc0e5532569f8521e15fd
vignan das <70110869+Vignan-das-chris@users.noreply.github.com>
2022-02-24 03:11:11 -0800

    Add files via upload

--- fabricjs/fabric.js
+++ fabricjs/fabric.js
@@ -5,7 +5,7 @@ document.getElementById('file').addEventListener("change",function(e){
     reader.onload=function(f){
         var data =f.target.result;
         fabric.Image.fromURL(data,function(img){
-            var oImg=img.set({left:0,angle:0,width:700,height:600}).scale(0.7);
+            var oImg=img.set({left:0,angle:0,width:700,height:700 }).scale(0.7);
             canvas.add(oImg).renderAll();
             var a= canvas.setActiveObject(oImg);
             var dataURL = canvas.toDataURL({format:'jpg',quality:0.8});
@@ -19,30 +19,34 @@ canvas.on('mouse:wheel',function(opt){
     var zoom=canvas.getZoom();
     zoom *=0.999 ** delta;
     if(zoom> 20) zoom=20;
-    if(zoom <0.01) zoom= 0.01;
-   canvas.zoomToPoint({ x:opt.e.offsetX, y:opt.e.offsetY},zoom);
-   opt.e.preventDefault();
-   opt.e.stopPropagation();
-   var vpt =this.viewportTransform;
-   if(zoom<400/1000){
-       vpt[4]=200-1000 * zoom / 2;
-       vpt[5]=200-1000 * zoom / 2;
-   }else{
-       if(vpt[4]>=0){
-           vpt[4]=0;
+    if(zoom < 1) zoom= 1;
+  canvas.zoomToPoint({x:opt.e.offsetX,y:opt.e.offsetY},zoom);
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+  var vpt=this.viewportTransform;
+  if(zoom<1400/1000){
+      vpt[4]=700-1000*zoom/2;
+      vpt[5]=700-1000*zoom/2;
+  }else{
 
-       }else if(vpt[4]<canvas.getWidth()-1000*zoom){
-           vpt[4]=canvas.getWidth()-1000*zoom;
-       }
-
-       if(vpt[5]>=0){
+    if(vpt[4]>=0){
+        vpt[4]=0;
+    }else if(vpt[4]<canvas.getWidth()-1000*zoom){
+        vpt[4]=canvas.getWidth()-1000*zoom;
+    }
+}
+    if(vpt[5]>=0){
         vpt[5]=0;
-
     }else if(vpt[5]<canvas.getHeight()-1000*zoom){
         vpt[5]=canvas.getHeight()-1000*zoom;
     }
-}
-   })
+  }
+ );
+
+
+
+
+
 
 
 
